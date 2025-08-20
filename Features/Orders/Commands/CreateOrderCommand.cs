4092c96682330fd685878eee2c023da235bb69a0
ShuRace <neupane16suresh@gmail.com>
2025-08-20 12:48:05 +0545

    Added CreateOrderCommand and GetOrderByIdQuery

---
+++ Features/Orders/Commands/CreateOrderCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace CqrsDemo.Features.Orders.Commands
+{
+    public class CreateOrderCommand : IRequest<int>
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }  
+
+    }
+}
---
+++ Features/Orders/Queries/GetOrderByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿// Features/Orders/Queries/GetOrderByIdQuery.cs
+using MediatR;
+
+namespace CqrsDemo.Features.Orders.Queries
+{
+    public class GetOrderByIdQuery : IRequest<OrderDto>
+    {
+        public int Id { get; set; }
+    }
+
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
